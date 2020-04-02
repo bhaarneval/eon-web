80e038d7c852821c47426c175368f3241896d873
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-03 03:54:13 +0530

    Updated dashboard with cards display.

--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -1,106 +1,54 @@
-import "./dashboard.less";
-
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import "./dashboard.css";
+import EventCards from "../../components/eventCards/Eventcards";
 
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import { withStyles } from "@material-ui/styles";
-
-import DashboardTable from "../../components/dashboardTable/dashboardTable";
-
-import {
-  DASHBOARD,
-  DEPLOYED_CLUSTERS,
-  RUNNING_CLUSTERS,
-  ACTIVE_SESSIONS,
-  INSTANCES
-} from "../../constants/constants";
-
-import cloud from "../../assets/cloud.png";
-import network from "../../assets/network.png";
-
-const styles = {
-  card: {
-    width: "23%",
-    height: 95,
-    borderRadius: "10px"
-  },
-  cardcontent: {
-    height: 65,
-    paddingBottom: "0 !important"
-  }
-};
-
+import { dummyList } from "../../constants/constants";
+import { Row } from "antd";
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventsList: []
+    };
+  }
+
+  spliceArray = list => {
+
+    let splicedList = [];
+    splicedList = list.reduce(
+      (rows, key, index) =>
+        (index % 4 == 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) &&
+        rows,
+      []
+    );
+    return splicedList.map((list,index) => {
+      return (
+        <Row key={index} className="cards-row">
+          {list.map((event, index) => {
+            return <EventCards key={index} event={event} />;
+          })}
+        </Row>
+      );
+    })
+  };
+
   render() {
-    const { classes } = this.props;
+    const list = dummyList
+    
     return (
-      <div>
-        <div className="headerLabel">{DASHBOARD}</div>
-        <div className="flex flex-row space-between dashboardHeader">
-          <Card className={classes.card} variant="outlined">
-            <CardContent className={classes.cardcontent}>
-              <div className="flex flex-row space-evenly cardContent">
-                <div className="cardImage cardImageBlue">
-                  <img className="img" src={network} />
-                </div>
-                <div className="flex flex-column cardInfo">
-                  <div className="cardInfoNo">05</div>
-                  <div className="cardDesc">{DEPLOYED_CLUSTERS}</div>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className={classes.card} variant="outlined">
-            <CardContent className={classes.cardcontent}>
-              <div className="flex flex-row space-evenly cardContent">
-                <div className="cardImage cardImageGreen">
-                  <img className="img" src={network} />
-                </div>
-                <div className="flex flex-column cardInfo">
-                  <div className="cardInfoNo">24</div>
-                  <div className="cardDesc">{RUNNING_CLUSTERS}</div>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className={classes.card} variant="outlined">
-            <CardContent className={classes.cardcontent}>
-              <div className="flex flex-row space-evenly cardContent">
-                <div className="cardImage cardImageOrange">
-                  <img className="img" src={cloud} />
-                </div>
-                <div className="flex flex-column cardInfo">
-                  <div className="cardInfoNo">03</div>
-                  <div className="cardDesc">{ACTIVE_SESSIONS}</div>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className={classes.card} variant="outlined">
-            <CardContent className={classes.cardcontent}>
-              <div className="flex flex-row space-evenly cardContent">
-                <div className="cardImage cardImageBlue">
-                  <img className="img" src={cloud} />
-                </div>
-                <div className="flex flex-column cardInfo">
-                  <div className="cardInfoNo">05</div>
-                  <div className="cardDesc">{INSTANCES}</div>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+      <>
+        <div className="events-heading"> Events Management </div>
+        <div className="events-container-flex">
+          {this.spliceArray(list)}
         </div>
-        <DashboardTable />
-      </div>
+      </>
     );
   }
 }
 
 Dashboard.propTypes = {
-  history: PropTypes.object,
-  classes: PropTypes.object
+  history: PropTypes.object
 };
 
-export default withStyles(styles)(Dashboard);
+export default Dashboard;
