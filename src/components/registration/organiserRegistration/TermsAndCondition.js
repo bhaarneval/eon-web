5883454ec0f8a5172e6d026076c44ac4458b33e4
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-02 11:03:02 +0530

    Updated Terms and Condition Modal

--- src/components/registration/organiserRegistration/TermsAndCondition.js
+++ src/components/registration/organiserRegistration/TermsAndCondition.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Modal, Checkbox, Button} from 'antd';
+import './organiserRegistration.css';
+import {termsConditions} from '../../../constants/termsConditions';
 
 export default function TermsAndConditions (props){
     const { handleAccept, isChecked, handleClose, handleCheckChange } = props;
@@ -9,13 +11,14 @@ export default function TermsAndConditions (props){
    <Modal
    visible
     onCancel = {handleClose}
-    title = {<h1 style = {{textAlign:'center', color: '#262C6F'}}>Terms & Conditions</h1>}
+    title = {<h1 className = 'modal-header'>Terms & Conditions</h1>}
     footer = {null}
+    className = 'modal-custom'
    >
-    <div>hello world!</div>
-    <div>
+    <div className = 'terms-conditions'>{termsConditions}</div>
+    <div className = 'modal-footer'>
         <Checkbox checked = {isChecked} onChange={handleCheckChange}>I hereby agree with the terms and conditions</Checkbox>
-        <Button style = {{backgroundColor:'#262C6F', color:'#ffffff'}} onClick = {handleAccept}>Accept</Button>
+        <Button className = 'accept-button' onClick = {handleAccept}>Accept</Button>
     </div>
    </Modal>
     )
