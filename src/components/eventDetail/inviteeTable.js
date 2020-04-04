26477a4a71fec78b16f5fbc5e8f6360d11344def
priyanka2k13 <priyanka.sah@hashedin.com>
2020-04-04 12:44:34 +0530

    table

---
+++ src/components/eventDetail/inviteeTable.js
@@ -0,0 +1,68 @@
+import { Table } from 'antd';
+import React from "react";
+import PropTypes from "prop-types";
+import {Button} from 'antd';
+
+const columns = [
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Contact',
+    dataIndex: 'contact',
+  },
+  {
+    title: 'Discount',
+    dataIndex: 'discount',
+  },
+];
+
+class EventTable extends React.Component {
+  state = {
+    selectedRowKeys: []
+  };
+
+  start = () => {
+    setTimeout(() => {
+      this.setState({
+        selectedRowKeys: [],
+      });
+    }, 1000);
+  };
+
+  onSelectChange = selectedRowKeys => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  };
+
+  render() {
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0;
+    return (
+      <div>
+        <div>{hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}</div>
+        {hasSelected && 
+            <Button type="primary" onClick={() => this.props.deleteAll(this.state.selectedRowKeys)}>
+                Delete All
+            </Button>}
+        <Table pagination={false} rowSelection={rowSelection} columns={columns} dataSource={this.props.data} />
+      </div>
+    );
+  }
+}
+
+EventTable.propTypes = {
+    data: PropTypes.object,
+    deleteAll: PropTypes.func
+};
+
+export default EventTable;
--- src/containers/event/eventDetail.js
+++ src/containers/event/eventDetail.js
@@ -5,25 +5,25 @@ import {Modal, Button, InputNumber, Input} from 'antd';
 const { TextArea } = Input;
 import EventInfo from "../../components/eventDetail/eventInfo";
 import EventCount from "../../components/eventDetail/eventCount";
+import EventTable from "../../components/eventDetail/inviteeTable";
 
 class InviteesPopup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showModal: true,
             inviteeList: {},
             count: 0,
             message:'',
             emailError:''
         }
-      }
+    }
 
-      validateEmail(email){      
-        var emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-        return emailPattern.test(email); 
-      } 
+    validateEmail(email){      
+    var emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailPattern.test(email); 
+    } 
 
-      handleKeyPress = (event) => {
+    handleKeyPress = (event) => {
         if(event.key === 'Enter'){
             event.preventDefault();
             if(this.validateEmail(event.target.value)){
@@ -79,14 +79,19 @@ class InviteesPopup extends Component {
                 <div>
                     <div className="email-row">
                         {Object.keys(this.state.inviteeList).map((key) => {
-                            return <span className="email" id={key} key={key}>{this.state.inviteeList[key]} <span onClick={() => this.onDelete(key)} className="delete-mark">x</span></span> 
+                            return <span className="email" id={key} key={key}>  {this.state.inviteeList[key]} 
+                                <span onClick={() => this.onDelete(key)} className="delete-mark">x</span>
+                            </span> 
                         })}
                     </div>
+                    Enter Invitee&apos;s Email ID
                     <TextArea 
                         value={this.state.message.length > 0 && this.state.message} 
                         rows={1} 
+                        placeholder="Press Enter to add email id"
                         onChange={this.onChange}
-                        onKeyPress={this.handleKeyPress} />
+                        onKeyPress={this.handleKeyPress} 
+                    />
                     {this.state.emailError.length > 0 &&
                         <div className="error">{this.state.emailError}</div>
                     }
@@ -114,7 +119,6 @@ class InviteesPopup extends Component {
                 </div>
             </Modal>
         )
-
     }
  }
  InviteesPopup.propTypes = {
@@ -123,11 +127,32 @@ class InviteesPopup extends Component {
     onDiscountChange: PropTypes.func.isRequired,
 }
 
+let data = [];
+for (let i = 0; i < 4; i++) {
+    data.push({
+    key: i,
+    email: `priyanka${i}@gmail.com`,
+    name: `Edward King${i}${i}${i}`,
+    contact: '1234567890',
+    discount: '10%'
+    });
+}
+data.push({
+    key: 4,
+    email: `priyankagmail.com`,
+    name: 'King2',
+    contact: '1234567890',
+    discount: '10%'
+    });
+
 class EventDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        showModal: true,
+        showModal: false,
+        rows: data,
+        searchValue: '',
+        filteredRows: []
     }
   }
 
@@ -144,30 +169,50 @@ class EventDetail extends Component {
 }
 
 
-handleAccept = () => {
-    
+deleteAll = (list) => {
+    const deletedList = []
+    const data = this.state.rows;
+    {list.map((no) => {
+        return deletedList.push(data[no]);
+    })}
+    console.log(deletedList)
 }
 
-onDiscountChange = () => {
-    
+handleSend = () => {
+}
+
+search = (event) => {
+    this.setState({
+        searchValue: event.target.value,
+        filteredRows: this.state.rows.filter((data) => {return data['name'].includes(event.target.value)})
+    })
 }
 
   render() {
+    console.log(this.state.rows)
     return (
       <div className="sub-content">
         <div className="events-heading">Event detail</div>
         <EventInfo />
         <EventCount />
-        Invitees List 
-        <Button type="primary" onClick={this.inviteButtonClick}>
-            Add Invitees
-        </Button>
+        <div className="invitee-row">
+            <b>Invitees List </b>
+            <Button type="primary" onClick={this.inviteButtonClick}>
+                Add Invitees
+            </Button>
+        </div>
+        <Input
+            placeholder="input search text"
+            onChange={event => this.search(event)}
+            style={{ width: 200 }}
+        />
+        <EventTable deleteAll={this.deleteAll} data={this.state.searchValue.length > 0 ? this.state.filteredRows : this.state.rows}/>
         {this.state.showModal &&
             <InviteesPopup
                 handleClose={this.handleModalClose}
                 handleSend={this.handleSend}
                 onDiscountChange={this.onDiscountChange}
-         />
+            />
         }
       </div>
     );
