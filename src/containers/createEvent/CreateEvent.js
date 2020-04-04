2afe888f5e481f7b2acc50fc4fabaf9584f63581
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-04 15:51:35 +0530

    Add container for create events.

---
+++ src/containers/createEvent/CreateEvent.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './createEvent.css';
+import BackButton from '../../components/BackButton';
+import EventForm from '../../components/eventCreation/eventForm';
+
+import PropTypes from 'prop-types'
+
+class CreateEvent extends Component {
+ state = {}
+ handleSubmit = (values) => {
+     let data = new FormData();
+     Object.entries(values).map(entry => {
+        data.append(entry[0],entry[1]);
+     });
+
+ }
+
+ goBack = () => {
+     this.props.history.push("/dashboard");
+ }
+
+
+ render() {
+  return (
+    <div className="create-container">
+      <div className="header">
+        <BackButton handleOnClick={this.goBack}/>
+        <div className="header-text">Create</div>
+      </div>
+      <div className="form-div">
+          <EventForm values = {{}} handleSubmit={this.handleSubmit} handleCancel={this.goBack}/>
+      </div>
+    </div>
+  );
+   }
+ }
+
+
+CreateEvent.propTypes = {
+    history: PropTypes.object.isRequired,
+}
+export default CreateEvent;
