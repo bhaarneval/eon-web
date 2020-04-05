847dba282e7773f86811e02ada9b063a51654f4d
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-05 15:52:16 +0530

    Updated the common backButton component to display the page header.

--- src/components/backButton.js
+++ src/components/backButton.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import backImg from '../assets/Back.svg';
+import React from "react";
+import PropTypes from "prop-types";
+import backImg from "../assets/Back.svg";
 
-export default function BackButton (props) {
-    const {handleOnClick} = props;
-    return (
-      <div>
-        <img
-          src={backImg}
-          onClick={handleOnClick}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-    );
+export default function BackButton(props) {
+  const { handleOnClick, text } = props;
+  return (
+    <div style = {styles.header}>
+      <img
+        src={backImg}
+        onClick={handleOnClick}
+        style={styles.backImage}
+      />
+      <div style={styles.headerText}>{text}</div>
+    </div>
+  );
 }
 BackButton.propTypes = {
-    handleOnClick:PropTypes.func.isRequired,
-}
+  handleOnClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired
+};
+const styles = {
+  header: {
+    display: "flex",
+    justifyContent: "flex-start",
+  },
+  headerText: {
+    color: "#262C6F",
+    fontWeight: "bold",
+    display: "flex",
+    justifyContent: "flex-start",
+    flexWrap:"nowrap",
+    width:"100%",
+    padding: "1%",
+    fontSize: "120%"
+  },
+  backImage: {
+    cursor: "pointer"
+  }
+};
