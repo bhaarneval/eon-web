315a28debb57a29497742d97853382f78dc5f96f
priyanka2k13 <priyanka.sah@hashedin.com>
2020-04-06 17:08:31 +0530

    user profile

--- src/containers/event/eventDetail.js
+++ src/containers/event/eventDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./eventDetail.css";
-import {Button, Input} from 'antd';
+import {Button, Input, InputNumber} from 'antd';
 import EventInfo from "../../components/eventDetail/eventInfo";
 import EventCount from "../../components/eventDetail/eventCount";
 import EventTable from "../../components/eventDetail/inviteeTable";
@@ -16,6 +16,13 @@ class EventDetail extends Component {
         searchValue: '',
         filteredRows: [],
         discount: '',
+        noOfSeats: 1,
+        totalAmount: 500,
+        totalAmountAfterPromo: 0,
+        intialAmount: 500,
+        discountPercentage: 10,
+        // role: 'Organizer'
+        role: 'User' //User
     }
   }
 
@@ -71,31 +78,83 @@ search = (event) => {
     })
 }
 
-  render() {
+onChangeSeats = (event) => {
+    console.log(event)
+}
+
+onIncDecSeats = (type) => {
+    if (type==='inc'){
+        this.setState({
+            noOfSeats : this.state.noOfSeats + 1,
+            totalAmount: (this.state.noOfSeats + 1) * this.state.intialAmount
+        })
+    }
+    else{
+        this.setState({
+            noOfSeats : this.state.noOfSeats - 1,
+            totalAmount: (this.state.noOfSeats - 1) * this.state.intialAmount
+        })
+    }
+}
+
+applyCode = () => {
+    this.setState({
+        totalAmountAfterPromo: this.state.totalAmount * (this.state.discountPercentage/100)
+    })
+}
+
+render() {
     console.log(this.state.rows)
     return (
       <div className="sub-content">
         <div className="events-heading">Event detail</div>
         <EventInfo history={this.props.history}/>
-        <EventCount />
-        <div className="invitee-row">
-            <h2><b>Invitees List</b></h2>
-            <Button type="primary" onClick={this.inviteButtonClick}>
-                Add Invitees
-            </Button>
-        </div>
-        <Input
-            placeholder="input search text"
-            onChange={event => this.search(event)}
-            style={{ width: 200, position: 'absolute', zIndex: 1 }}
-        />
-        <EventTable deleteAll={this.deleteAll} data={this.state.searchValue.length > 0 ? this.state.filteredRows : this.state.rows}/>
-        {this.state.showModal &&
-            <InviteesPopup
-                handleClose={this.handleModalClose}
-                handleSend={this.handleSend}
-                onDiscountChange={this.onDiscountChange}
-            />
+        {this.state.role === 'Organizer' &&
+            <div>
+                <EventCount />
+                <div className="invitee-row">
+                    <h2><b>Invitees List</b></h2>
+                    <Button type="primary" onClick={this.inviteButtonClick}>
+                        Add Invitees
+                    </Button>
+                </div>
+                <Input
+                    placeholder="input search text"
+                    onChange={event => this.search(event)}
+                    style={{ width: 200, position: 'absolute', zIndex: 1 }}
+                />
+                <EventTable deleteAll={this.deleteAll} data={this.state.searchValue.length > 0 ? this.state.filteredRows : this.state.rows}/>
+                {this.state.showModal &&
+                    <InviteesPopup
+                        handleClose={this.handleModalClose}
+                        handleSend={this.handleSend}
+                        onDiscountChange={this.onDiscountChange}
+                    />
+                }
+            </div>
+        }
+        {this.state.role === 'User' &&
+            <div className="detail-card">
+                <Button type="primary" onClick={() => this.onIncDecSeats('dec')}>-</Button>
+                    <InputNumber
+                        min={1}
+                        defaultValue={1}
+                        onChange={event => this.onChangeSeats(event)}
+                    />
+                <Button type="primary" onClick={() => this.onIncDecSeats('inc')}>+</Button>
+                Updated amount
+                <InputNumber
+                    min={1}
+                    disabled
+                    value={this.state.totalAmount}
+                />
+                <div>
+                    Promotional Offers
+                    <div>10% is available for you <Button type="primary" onClick={this.applyCode}>Apply</Button></div>
+                    Total amount
+                    {this.state.totalAmountAfterPromo}
+                </div>
+            </div>
         }
       </div>
     );
