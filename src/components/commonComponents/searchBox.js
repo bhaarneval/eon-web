f5e03cf7d5927dcfa47015fb7462b48e883719a0
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-16 12:50:45 +0530

    search box change.

--- src/components/commonComponents/searchBox.js
+++ src/components/commonComponents/searchBox.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Proptypes from "prop-types";
-import {Select} from "antd";    
+import {Input} from "antd";    
 
 import searchImg from '../../assets/Search.svg';
 
 export default function SearchBox (props) {
-    const {handleKeyPress, placeholder} = props;
+    const {handleKeyPress, placeholder, value} = props;
+    let [currentValue, setValue] =useState(value);
+
+    useEffect(() => {
+      setValue(value)
+    }, [value])
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
     return (
-      <Select
-        mode="multiple"
-        size="large"
-        style={{ width: "30%"}}
-        notFoundContent={null}
+      <Input
         placeholder={placeholder}
-        suffixIcon={<img src={searchImg}/>}
-        // onSearch={handleOnChange}
-        onKeyPress = {handleKeyPress}
+        onPressEnter={(event) => handleKeyPress(event)}
+        style={{ width: 200, zIndex: 1 }}
+        prefix={<img src={searchImg}/>}
+        allowClear={true}
+        onChange = {handleChange}
+        value={currentValue}
       />
     );
 
@@ -23,4 +32,5 @@ export default function SearchBox (props) {
 SearchBox.propTypes = {
     handleKeyPress: Proptypes.func.isRequired,
     placeholder: Proptypes.string.isRequired,
+    value: Proptypes.string,
 }
