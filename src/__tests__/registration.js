7b95aca5fb7d4e3fe478642b41512017d908dca2
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-20 17:00:44 +0530

    Registration code coverage 100% achieved.

--- src/__tests__/registration.js
+++ src/__tests__/registration.js
@@ -5,23 +5,31 @@ import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import sinon from "sinon";
 import UserDetails from "../components/registration/userRegistration/forms";
-import {BasicDetails} from "../components/registration/organiserRegistration/forms";
-import {Form, Input} from "antd"
+import {
+  BasicDetails,
+  PasswordDetails,
+} from "../components/registration/organiserRegistration/forms";
+import StyledButtons from "../components/registration/styledButtons";
+import FormSteps from "../components/registration/formSteps";
+import TermsAndConditions from "../components/registration/termsAndConditions";
+import { Form, Input, Button, Modal } from "antd";
 
 const handleSubmit = sinon.spy();
 const handleChangePassword = sinon.spy();
-const userDetails = (hasErrored) =>{return (
-  <UserDetails
-    values={{}}
-    handleSubmit={handleSubmit}
-    handlePasswordChange={handleChangePassword}
-    currentPassword={""}
-    hasErrored={hasErrored? hasErrored: false}
-    errorMessage={""}
-  />
-);}
+const userDetails = (hasErrored) => {
+  return (
+    <UserDetails
+      values={{}}
+      handleSubmit={handleSubmit}
+      handlePasswordChange={handleChangePassword}
+      currentPassword={""}
+      hasErrored={hasErrored ? hasErrored : false}
+      errorMessage={""}
+    />
+  );
+};
 configure({ adapter: new Adapter() });
-describe("<UserDetails /> components", () => {
+describe("User Registration components", () => {
   it("find the numer of Form components", () => {
     const wrapper = shallow(userDetails());
     expect(wrapper.find(".form-main")).toHaveLength(1);
@@ -42,25 +50,34 @@ describe("<UserDetails /> components", () => {
   });
   it("Number of input components", () => {
     const wrapper = shallow(userDetails());
-    
+
     expect(wrapper.find(Form.Item)).toHaveLength(5);
   });
   it("Number of password input div", () => {
     const wrapper = shallow(userDetails());
-    
+
     expect(wrapper.find(Input.Password)).toHaveLength(2);
   });
-
 });
 
+const basicDetails = () => {
+  return <BasicDetails values={{}} handleSubmit={handleSubmit} />;
+};
 
-const basicDetails = () =>{return (
-  <BasicDetails
-    values={{}}
-    handleSubmit={handleSubmit}
-  />
-);}
-
+const handleBack = sinon.spy();
+const passwordDetails = (hasErrored) => {
+  return (
+    <PasswordDetails
+      handleSubmit={handleSubmit}
+      values={{}}
+      handleBack={handleBack}
+      handlePasswordChange={handleChangePassword}
+      currentPassword={""}
+      hasErrored={hasErrored ? hasErrored : false}
+      errorMessage={""}
+    />
+  );
+};
 describe("Organizer registration form components", () => {
   it("find the numer of Form components", () => {
     const wrapper = shallow(basicDetails());
@@ -72,8 +89,56 @@ describe("Organizer registration form components", () => {
   });
   it("Number of input components", () => {
     const wrapper = shallow(basicDetails());
-    
+
     expect(wrapper.find(Form.Item)).toHaveLength(4);
   });
+  it("Number of password input div", () => {
+    const wrapper = shallow(passwordDetails());
 
+    expect(wrapper.find(Input.Password)).toHaveLength(2);
+  });
+  it("Number of error message div", () => {
+    const wrapper = shallow(passwordDetails(true));
+
+    expect(wrapper.find(".error-message")).toHaveLength(1);
+  });
+});
+
+const styledButtons = () => {
+  return <StyledButtons content={<div>Next</div>} onClick={handleSubmit} />;
+};
+describe("styledButtons components", () => {
+  it("number of button components", () => {
+    const wrapper = shallow(styledButtons());
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+});
+
+const formSteps = () => {
+  return <FormSteps stepList={["Basic", "Advanced"]} activeKey={0} />;
+};
+describe("FormSteps components", () => {
+  it("number of components", () => {
+    const wrapper = shallow(formSteps());
+    expect(wrapper.find(".multistep-style")).toHaveLength(1);
+  });
+});
+
+const handleModalClose = sinon.spy();
+const handleCheckBoxChnage = sinon.spy();
+const termsAndConditionModal = (isChecked) => {
+  return (
+    <TermsAndConditions
+      isChecked={isChecked ? isChecked : false}
+      handleClose={handleModalClose}
+      handleAccept={handleSubmit}
+      handleCheckChange={handleCheckBoxChnage}
+    />
+  );
+};
+describe("Terms and condition modal components", () => {
+  it("number of components", () => {
+    const wrapper = shallow(termsAndConditionModal());
+    expect(wrapper.find(Modal)).toHaveLength(1);
+  });
 });
