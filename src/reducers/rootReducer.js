1b20f8bd8cb1eac4af23b300baa823bb867e3e55
mayankumar07 <mayank.kumar@hashedin.com>
2020-04-22 15:17:45 +0530

    Add reducers for analytics.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,13 +2,15 @@ import clusterReducer from "./clusterReducer";
 import userReducer from "./userReducer";
 import eventReducer from "./eventReducer";
 import notificationReducer from "./notificationReducer";
+import analyticsReducer from "./analyticsReducer"
 import { combineReducers } from "redux";
 
 const appReducer = combineReducers({
   clusterReducer,
   userReducer,
   eventReducer,
-  notificationReducer
+  notificationReducer,
+  analyticsReducer
 });
 
 const rootReducer = (state, action) => {
